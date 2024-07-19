932be2893dc767d86bfdc84c48c825a1fb089e3b
FloresTM <seb.flores2002@gmail.com>
2024-07-20 08:47:20 +1000

    rebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,10 +15,16 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectItem } from "@/components/ui/select"; // Assuming you have a Select component
-import { SelectContent, SelectTrigger, SelectValue } from "@radix-ui/react-select";
+import {
+  SelectContent,
+  SelectTrigger,
+  SelectValue,
+} from "@radix-ui/react-select";
 import { Category, User } from "@prisma/client";
 import UserSection from "@/components/UserSection";
-import { PlusCircle } from "lucide-react";
+import { LogOut, PlusCircle } from "lucide-react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const [title, setTitle] = useState("");
@@ -33,23 +39,23 @@ export default function Home() {
     try {
       const response = await fetch("/api/users");
       const data = await response.json();
-      console.log('Users:', data);
+      console.log("Users:", data);
       setUsers(data);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error("Error fetching users:", error);
     }
-  }
+  };
 
   const fetchCategories = async () => {
     try {
       const response = await fetch("/api/categories");
       const data = await response.json();
-      console.log('Categories:', data);
+      console.log("Categories:", data);
       setCategories(data);
     } catch (error) {
-      console.error('Error fetching categories:', error);
+      console.error("Error fetching categories:", error);
     }
-  }
+  };
 
   const handleSubmit = async () => {
     try {
@@ -66,15 +72,15 @@ export default function Home() {
           categoryId: parseInt(categoryId),
         }),
       });
-  
+
       if (response.ok) {
-        console.log('Expense created successfully');
+        console.log("Expense created successfully");
       } else {
         const errorData = await response.json();
-        console.error('Error creating expense:', errorData);
+        console.error("Error creating expense:", errorData);
       }
     } catch (error) {
-      console.error('Error submitting form:', error);
+      console.error("Error submitting form:", error);
     }
   };
 
@@ -83,8 +89,15 @@ export default function Home() {
     fetchCategories();
   }, []);
 
+  const router = useRouter();
+
+  const handleLogout = () => {
+    Cookies.remove("isAuthenticated");
+    router.push("/login");
+  };
+
   return (
-    <main className="p-4 flex flex-col h-screen">
+    <main className="p-4 flex gap-2 flex-col h-screen">
       <Card className="bg-primary">
         <CardHeader className="px-6">
           <CardTitle className="text-white">Expenses</CardTitle>
@@ -102,99 +115,54 @@ export default function Home() {
           </Card>
         </CardContent>
       </Card>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="bg-primary py-8 w-full my-2">
-            <PlusCircle className="text-white mr-3" />
-            Create Expense
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Create Expense</DialogTitle>
-            <DialogDescription>
-              Fill in the details to add a new expense.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="title" className="text-right">
-                Title
-              </Label>
-              <Input
-                id="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="amount" className="text-right">
-                Amount
-              </Label>
-              <Input
-                id="amount"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                className="col-span-3"
-                type="number"
-                step="0.01"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="description" className="text-right">
-                Description
-              </Label>
-              <Input
-                id="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                className="col-span-3"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="user" className="text-right">
-                User
-              </Label>
-              <Select onValueChange={(value) => setUserId(value)}>
-                <SelectTrigger className="col-span-3">
-                  <SelectValue placeholder="Select a user" />
-                </SelectTrigger>
-              <SelectContent>
-                {users.map((user) => (
-                  <SelectItem key={user.id} value={user.id.toString()}>
-                    {user.name}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="category" className="text-right">
-                Category
-              </Label>
-              <Select onValueChange={(value) => setCategoryId(value)}>
-                <SelectTrigger className="col-span-3">
-                  <SelectValue placeholder="Select a category" />
-                </SelectTrigger>
-                <SelectContent>
-                  {categories.map((category) => (
-                    <SelectItem key={category.id} value={category.id.toString()}>
-                      {category.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent> 
-              </Select>
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" onClick={handleSubmit}>
-              Save Expense
+      <div className="flex gap-2 items-center h-16">
+        <Button className="w-full h-full" onClick={handleLogout}>
+          <LogOut className="w-5 h-5 mr-3" />
+          Logout
+        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="bg-primary py-8 w-full h-full">
+              <PlusCircle className="text-white w-5 h-5 mr-3" />
+              Create Expense
             </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full">
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full pb-3">
         <UserSection user="Randu" />
         <UserSection user="Flores" />
       </section>
